ce224727c97a8ac4907b843477d920c8dca28a45
Alejandro <alehdzclr@gmail.com>
2025-05-16 10:09:09 +0100

    Base de node.js

---
+++ public/js/incluir_partials.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('/partials/nav.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('nav').innerHTML = data;
+    });
+
+  fetch('/partials/footer.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
+    });
+});
